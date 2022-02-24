7679efbfbe3b20736f1614de3e47e7b4e1ff1596
escc1122 <escc1122@gamil.com>
2022-02-24 16:53:18 +0800

    Add Mini-MaxSum

---
+++ Mini-MaxSum/main_test.go
@@ -0,0 +1,9 @@
+package main
+
+func Example_miniMaxSum() {
+	testArray := []int32{256741038, 623958417, 467905213, 714532089, 938071625}
+	miniMaxSum(testArray)
+	// Output:
+	// 2063136757 2744467344
+
+}
