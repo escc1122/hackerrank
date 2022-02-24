0390853ad446be0f79ebd70e24cc3c02cf25d3a9
escc1122 <escc1122@gamil.com>
2022-02-24 23:12:01 +0800

    Add SparseArrays

---
+++ SparseArrays/main_test.go
@@ -0,0 +1,11 @@
+package main
+
+func Example_matchingStrings() {
+	testArray := []string{"-4", "3", "-9", "0"}
+	matchingStrings(testArray, testArray)
+	// Output:
+	// 0.5
+	//0.33333334
+	//0.16666667
+
+}
