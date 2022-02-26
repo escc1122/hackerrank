f093c0565255c68b350fab17621384d534e2818b
escc1122 <escc1122@gamil.com>
2022-02-26 17:16:40 +0800

    Add ZigZagSequence

---
+++ ZigZagSequence/src/test/java/MainTest.java
@@ -0,0 +1,15 @@
+import org.junit.Test;
+
+public class MainTest {
+
+    @Test
+    public void findZigZagSequence(){
+
+        System.out.println("test");
+        int[] a = {1 ,2 ,3, 4, 5, 6 ,7};
+        Main.findZigZagSequence(a,7);
+    }
+}
+
+
+
